e077139344fbcfb2d3bfe9fce3ad3134b73d9ed1
veerabhadraswamy17 <veerabhadraswamy@outlook.com>
2023-11-22 21:44:57 +0000

    linux node pool name error fix

--- terraform-manifests/09-aks-linux-nodepools.tf
+++ terraform-manifests/09-aks-linux-nodepools.tf
@@ -5,7 +5,7 @@ resource "azurerm_kubernetes_cluster_node_pool" "linux_nodepool_101" {
     max_count = 1
     min_count = 1
     mode = "User"
-    name = linux_nodepool_101
+    name = "linux_nodepool_101"
     orchestrator_version = data.azurerm_kubernetes_service_versions.current.latest_version
     os_disk_size_gb = 30
     os_type = "Linux"
