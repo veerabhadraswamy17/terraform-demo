d8d330888b1df4e9dd0efbb656eed8c1e90ca5d9
veerabhadraswamy17 <veerabhadraswamy@outlook.com>
2023-11-22 21:52:16 +0000

    linux node pool name error fix

--- terraform-manifests/09-aks-linux-nodepools.tf
+++ terraform-manifests/09-aks-linux-nodepools.tf
@@ -1,11 +1,11 @@
-resource "azurerm_kubernetes_cluster_node_pool" "linux_nodepool_101" {
+resource "azurerm_kubernetes_cluster_node_pool" "linux_np_101" {
     zones = [1, 2, 3]
     enable_auto_scaling = true
     kubernetes_cluster_id = azurerm_kubernetes_cluster.aks-cluster.id
     max_count = 1
     min_count = 1
     mode = "User"
-    name = "linux_nodepool_101"
+    name = "linux_np_101"
     orchestrator_version = data.azurerm_kubernetes_service_versions.current.latest_version
     os_disk_size_gb = 30
     os_type = "Linux"
--- terraform-manifests/10-aks-windows-nodepools.tf
+++ terraform-manifests/10-aks-windows-nodepools.tf
@@ -1,11 +1,11 @@
-resource "azurerm_kubernetes_cluster_node_pool" "windows_nodepool_101" {
+resource "azurerm_kubernetes_cluster_node_pool" "win_np_101" {
     zones = [1, 2, 3]
     enable_auto_scaling = true
     kubernetes_cluster_id = azurerm_kubernetes_cluster.aks-cluster.id
     max_count = 1
     min_count = 1
     mode = "User"
-    name = "windows_nodepool_101"
+    name = "win_np_101"
     orchestrator_version = data.azurerm_kubernetes_service_versions.current.latest_version
     os_disk_size_gb = 60
     os_type = "Windows"
