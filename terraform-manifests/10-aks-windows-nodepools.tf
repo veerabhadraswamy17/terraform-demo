b50648c9885945dd19bd3202659d5484650dc5bf
veerabhadraswamy17 <veerabhadraswamy@outlook.com>
2023-11-22 23:03:02 +0000

    disabled linux and windows nodepools

--- terraform-manifests/10-aks-windows-nodepools.tf
+++ terraform-manifests/10-aks-windows-nodepools.tf
@@ -1,26 +1,26 @@
-resource "azurerm_kubernetes_cluster_node_pool" "win_np_101" {
-    zones = [1, 2, 3]
-    enable_auto_scaling = true
-    kubernetes_cluster_id = azurerm_kubernetes_cluster.aks-cluster.id
-    max_count = 1
-    min_count = 1
-    mode = "User"
-    name = "winnp101"
-    orchestrator_version = data.azurerm_kubernetes_service_versions.current.latest_version
-    os_disk_size_gb = 60
-    os_type = "Windows"
-    vm_size = "Standard_DS2_v2"
-    priority = "Regular"
-    node_labels = {
-      "nodepool-type" = "user"
-      "environment" = var.environment
-      "nodepoolos" = "windows"
-      "app" = "dotnet-apps"      
-    }
-    tags = {
-      "nodepool-type" = "user"
-      "environment" = var.environment
-      "nodepoolos" = "windows"
-      "app" = "dotnet-apps"      
-    }
-}
+# resource "azurerm_kubernetes_cluster_node_pool" "win_np_101" {
+#     zones = [1, 2, 3]
+#     enable_auto_scaling = true
+#     kubernetes_cluster_id = azurerm_kubernetes_cluster.aks-cluster.id
+#     max_count = 1
+#     min_count = 1
+#     mode = "User"
+#     name = "winnp101"
+#     orchestrator_version = data.azurerm_kubernetes_service_versions.current.latest_version
+#     os_disk_size_gb = 60
+#     os_type = "Windows"
+#     vm_size = "Standard_DS2_v2"
+#     priority = "Regular"
+#     node_labels = {
+#       "nodepool-type" = "user"
+#       "environment" = var.environment
+#       "nodepoolos" = "windows"
+#       "app" = "dotnet-apps"      
+#     }
+#     tags = {
+#       "nodepool-type" = "user"
+#       "environment" = var.environment
+#       "nodepoolos" = "windows"
+#       "app" = "dotnet-apps"      
+#     }
+# }
